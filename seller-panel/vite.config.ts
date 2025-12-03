803fcaab7930cd7922bb0270ac14a6aa01ecbb77
Abdul Aziz Ahmed <azizadnan370@gmail.com>
2025-12-04 03:42:48 +0530

    added url

--- seller-panel/vite.config.ts
+++ seller-panel/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/seller-panel/',
+  base: '/seller/',
   plugins: [react()],
   resolve: {
     alias: {
