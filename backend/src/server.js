9a5f88d85aaefa3e66734e20b5dc9a050d5ab446
Abdul Aziz Ahmed <azizadnan370@gmail.com>
2025-12-04 04:59:59 +0530

    api url

--- backend/src/server.js
+++ backend/src/server.js
@@ -228,6 +228,7 @@ const corsOptions = {
     "http://localhost:3001",
     "https://b2b-v1-seller.vercel.app",
     "https://b2b-v1-admin.vercel.app",
+    "https://b2b-v1-storefront.vercel.app",
     process.env.CLIENT_URL,
     process.env.ADMIN_URL,
     process.env.SELLER_URL
